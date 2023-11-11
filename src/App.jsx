7bab7aa506ef5454e267db18732b8f9c5d734c79
eryc_s.z.w <erycszw10@gmail.com>
2023-11-11 12:25:55 +0700

    initialize repo

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="h-screen text-white text-lg font-bold flex items-center justify-center bg-gray-700">
+      <p className='text-center'>Welcome to identifEye!</p>
+    </div>
+  )
+}
+
+export default App
